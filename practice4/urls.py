2dd3204d9084a4865eeacf7976fee8f82b7dcd18
abdullah <abdullah15-12557@diu.edu.bd>
2022-02-19 03:42:05 +0600

    module

--- practice4/urls.py
+++ practice4/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path as pa
+from django.urls import include
 from django.views.static import serve
 from django.conf import settings
 
 from app import urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    pa('admin/', admin.site.urls),
+    pa('', include('app.urls')),
 
     urls(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     urls(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
