a2f8081762dabbe7efae7bdd18cceccd2ce23510
abdullah <abdullah15-12557@diu.edu.bd>
2022-02-19 02:49:12 +0600

    s

--- practice4/settings.py
+++ practice4/settings.py
@@ -114,8 +114,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "staticfiles"
