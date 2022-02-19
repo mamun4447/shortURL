55f0f3bd7e1fdd10a674d25c700fed9e4d158a29
abdullah <abdullah15-12557@diu.edu.bd>
2022-02-19 13:36:28 +0600

    c

--- practice4/urls.py
+++ practice4/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
 
     # urls(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # urls(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]   
